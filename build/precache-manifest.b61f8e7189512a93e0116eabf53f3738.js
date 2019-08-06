30247e227f6d646e0aac9631d131ec307ac87a39
karanjagota <karan.02031993@gmail.com>
2019-08-06 21:27:15 +0530

    add build app

---
+++ build/precache-manifest.b61f8e7189512a93e0116eabf53f3738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8716b0b303b23f5f0050701d207408b",
+    "url": "/react-sample-app/index.html"
+  },
+  {
+    "revision": "36bdd80596e97f1ffcdd",
+    "url": "/react-sample-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3907af2b625f477a9d48",
+    "url": "/react-sample-app/static/js/2.61372972.chunk.js"
+  },
+  {
+    "revision": "36bdd80596e97f1ffcdd",
+    "url": "/react-sample-app/static/js/main.97704cb8.chunk.js"
+  },
+  {
+    "revision": "662c07ef70e8403ce9ec",
+    "url": "/react-sample-app/static/js/runtime~main.04716803.js"
+  }
+]);
